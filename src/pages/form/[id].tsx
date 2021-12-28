1e32d1f35c00c17894d4bf49d47a49108d148636
Angelo Reis <angelo.desenvolvedor@gmail.com>
2021-12-27 21:50:46 -0300

    fix: ref value on form

--- src/pages/form/[id].tsx
+++ src/pages/form/[id].tsx
@@ -1,21 +1,16 @@
 import React, { useState, useEffect, FormEvent } from 'react';
 import { GetServerSideProps } from 'next';
+
 import Link from 'next/link';
 
 import { FaHome, FaCheckSquare } from 'react-icons/fa';
 
-import { useToast, Heading, Flex, Button, SimpleGrid, Icon } from '@chakra-ui/react';
+import { useToast, Heading, Flex, Button, SimpleGrid, Icon, useControllableState, useControllableProp } from '@chakra-ui/react';
 import { userResponse } from '../../utils/types';
 import { showToastStatus } from '../../components/Toasty';
 import { Input } from '../../components/Form/Input'
 
-import { yupResolver } from '@hookform/resolvers/yup';
-import * as Yup from 'yup'
-
 import { api } from '../../services/api';
-import { useForm } from 'react-hook-form';
-import { Console } from 'console';
-
 interface UserProps {
   user: userResponse;
 }
@@ -24,25 +19,21 @@ export default function User({ user }: UserProps) {
   const toast = useToast()
   const [userData, setUserData] = useState<userResponse>({} as userResponse);
 
-  const validationSchema = Yup.object().shape({
-    name: Yup.string().required('Name is required'),
-    username: Yup.string().required('userName is required'),
-    email: Yup.string().required('Email required'),
-  })
+  const [internalUserData, setInternalUserData] = useControllableState({
+    defaultValue: userData,
+    onChange: setUserData
+  });
 
-  const formOptions = { resolver: yupResolver(validationSchema) };
 
-  const { register, handleSubmit, reset, formState } = useForm(formOptions)
-  const { errors } = formState
 
-  function onSubmit(event) {
-    async function saveUser() {
-      const response = await api.put(`/users/${userData.id}`, { body: userData });
-      setUserData(response.data);
-      showToastStatus(response.status, toast);
-    }
-    saveUser();
+  function onSubmit(event: FormEvent<HTMLFormElement>) {    
     event.preventDefault();
+      api.put(`/users/${userData.id}`, { body: userData })
+         .then((response) => {
+            const {data, status} = response;
+            setInternalUserData(data.body);
+            showToastStatus(status, toast);    
+      })
   }
 
   const onChangeValueField = (event: FormEvent<HTMLInputElement>) => {
@@ -50,8 +41,7 @@ export default function User({ user }: UserProps) {
     const { id: key, value } = currentTarget
     const newUserData = {...userData}
     newUserData[key] = value
-    setUserData(newUserData)
-    console.log('onChangeValueField',userData)
+    setInternalUserData(newUserData)
   }
 
   const onChangeValueFieldAddress = (event: FormEvent<HTMLInputElement>) => {
@@ -59,25 +49,24 @@ export default function User({ user }: UserProps) {
     const { id: key, value } = currentTarget
     const newUserData = { ...userData }
     newUserData.address[key] = value;
-    setUserData(newUserData)
-    console.log('onChangeValueFieldAddress', userData)
+    setInternalUserData(newUserData)
   }
 
-  useEffect(() => setUserData(user), [user]);
-
+  useEffect(() => setInternalUserData(user), [user]);
 
   return (
     <Flex direction='column'>
-      <Heading mx='20' my='10' py='5' px='10' >
-        Formulário de Edição do Usuário: {userData?.name}
-      </Heading>
+      
       <SimpleGrid bg='white' color='gray.900' mx='20' my='10' py='5' px='10' borderRadius={5}>
-        <form onSubmit={handleSubmit(onSubmit)}>
+      <Heading mx='20' my='10' py='5' px='10' >
+        Formulário de Edição do Usuário: {userData.name}
+      </Heading>
+        <form onSubmit={onSubmit}>
           <Input label='id' type="text" name="id" id="id" value={userData.id} onChange={onChangeValueField} />
-          <Input label='Nome' type="text" name="name" id="name" value={userData.name} onChange={onChangeValueField} />
-          <Input label='Usuário' type="text" name="username" id="username" value={userData.username} onChange={onChangeValueField} />
+          <Input label='Nome' type="text" name="name" id="name"  value={userData.name} onChange={onChangeValueField} />
+          <Input label='Usuário' type="text" name="username" id="username"  value={userData.username} onChange={onChangeValueField} />
 
-          <Input label='E-mail' type="email" name="email" id="email" value={userData.email} onChange={onChangeValueField} />
+          <Input label='E-mail' type="email" name="email" id="email"  value={userData.email} onChange={onChangeValueField} />
           <Input label='Telefone' type="text" name="phone" id="phone" value={userData.phone} onChange={onChangeValueField} />
           <Input label='Site' type="text" name="website" id="website" value={userData.website} onChange={onChangeValueField} />
 
@@ -92,7 +81,6 @@ export default function User({ user }: UserProps) {
                 <p>Voltar para Home</p>
               </Link>
             </Button>
-
             <Button bg='green.400' leftIcon={<Icon as={FaCheckSquare} />} _hover={{ bg: 'green.600' }} type='submit'>
               Salvar
             </Button>
